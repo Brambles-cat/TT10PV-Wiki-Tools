96f51c43392dd92e593a783b547ee081d247ee90
Brambles-cat <gael.alejos05@gmail.com>
2024-12-19 10:52:25 -0800

    Add page update functionality

--- archive_to_wiki.py
+++ archive_to_wiki.py
@@ -1,4 +1,4 @@
-from modules.external import get_archive
+from modules.external import get_archive, ArchiveIndices as I
 from modules.wiki import Wiki
 import os, dotenv
 
@@ -11,7 +11,21 @@ wiki = Wiki(wiki_api_key, domain)
 
 archive_rows = get_archive(archive_url)
 
+base_template = "templates/t10video_base.html"
+updated_template = "templates/t10video_updated.html"
+
+
+if True:
+    for i, row in enumerate(archive_rows[:3]):
+        wiki.create_top10v_page(row, base_template)
+        print(f"wiki page #{i+1} made")
+
+input(".")
+
+t10v_ids = wiki.get_top10v_ids()
 
 for i, row in enumerate(archive_rows[:3]):
-    wiki.create_top10v_page(row)
-    print(f"wiki page #{i+1} made")
+    ids = next(ids for ids in t10v_ids if ids["path"].endswith(wiki._path_sanitize(row[I.TITLE])))
+
+    wiki.update_top10v_page(ids["id"], row, updated_template)
+    print(f"wiki page {ids["path"]} updated")
--- modules/query_templates.py
+++ modules/query_templates.py
@@ -19,5 +19,45 @@ _create_page = """mutation {{
     }}
 }}"""
 
+_update_page = """mutation {{
+    pages {{
+        update(
+            id: {0}
+            content: "{1}"
+            scriptCss: "{2}"
+            isPublished: true
+        ) {{
+            page {{
+                id
+            }}
+        }}
+    }}
+}}"""
+
+_get_page = """query {{
+    pages {{
+        single(id: {0}) {{
+            content
+            scriptCss
+        }}
+    }}
+}}"""
+
+get_page_ids = """query {
+    pages {
+        list(orderBy: PATH, orderByDirection: DESC) {
+            path
+            id
+        }
+    }
+}"""
+
+
 def create_page(content: str, description: str, css: str, path: str, title: str):
     return _create_page.format(content, description, path, css, title)
+
+def update_page(page_id: int, content: str, css: str):
+    return _update_page.format(page_id, content, css)
+
+def get_page(page_id):
+    return _get_page.format(page_id)
